044ac626f478386b5c90c0096e01cb5028a4bfc7
Shantelle Toh <shantelleschool@gmail.com>
2024-04-27 11:25:01 -0700

    add rest of func defs for remaining reqs of asgn

--- ta2_grading_script.py
+++ ta2_grading_script.py
@@ -63,13 +63,78 @@ def check_tooltip():
         print("The HTML file does not contain any <h1> tags.")
 
 
+# 4. Colored background (other than white) (100 points)
+def check_colored_background():
+    pass
+
+
+# 5. "Mailto" link to your email address (50 points)
+def check_mailto():
+    pass
+
+
+# 6. Hyperlink to another website (must use a URL, not a file path) (50 points)
+def check_hyperlink():
+    pass
+
+
+# 7. Bold text and italic text (50 points each)
+def check_bold_and_italic():
+    pass
+
+
+# 8. Centered text or photo (50 points)
+def check_centered_text_or_photo():
+    pass
+
+
+# 9. Horizontal line (aka "horizontal rule") (50 points)
+def check_horizontal_line():
+    pass
+
+
+# 10. Ordered list (numbered list) and unordered list (bulleted list) (50 points each)
+def check_ordered_and_unordered_list():
+    pass
+
+
+# 11. A working picture, hosted online. You can link to an existing photo or upload your own image to photo-sharing sites like Google Photos or imgur.com. Make sure the photo is shared properly, and test your page on someone else's device to be certain. (100 points)
+def check_picture():
+    pass
+
+
+# 12. Set the width and height of your photo. Recommend 450x600 for portrait layout, or 600x450 for landscape. If your source photo isn't 4:3 aspect ratio, this may look odd. (100 points)
+def check_picture_width_height():
+    pass
+
+
+# 13. Add a comment at the very bottom of your source code, listing the tools you used to create this: operating system, text editor, and the web browser you used to test it. (100 points)
+def check_comment():
+    pass
+
+
+# Extra Credit: Add a video to your page, using YouTube, Vimeo, or other video hosting service. Find a video, look for the "Share" option, and select "Embed" or "Link" to find a working video link.
+def check_extra_credit_video():
+    pass
+
+
 if __name__ == "__main__":
     file_path = input("Enter the path to the HTML file: ")
 
     soup = check_type_html(file_path)
     if soup:
-        check_title()
-        check_headings()
-        check_tooltip()
+        check_title() # 1.
+        check_headings() # 2.
+        check_tooltip() # 3.
+        check_colored_background() # 4.
+        check_mailto() # 5.
+        check_hyperlink() # 6.
+        check_bold_and_italic() # 7.
+        check_centered_text_or_photo() # 8.
+        check_horizontal_line() # 9.
+        check_ordered_and_unordered_list() # 10.
+        check_picture() # 11.
+        check_picture_width_height() # 12.
+        check_comment() # 13.
+        check_extra_credit_video()
     
-    print("\n\n\n")
