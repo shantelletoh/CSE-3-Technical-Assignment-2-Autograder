03401778c3f85b268098217edbb94efe6da45e47
Shantelle Toh <shantelleschool@gmail.com>
2024-04-28 14:06:59 -0700

    write student feedback and grader msg to csv, update formatting, bit more bug fixes

--- ta2_grading_script.py
+++ ta2_grading_script.py
@@ -7,11 +7,14 @@ import csv
 
 # check for correct html file type
 def check_type_html(file_name):
+    global student_feedback
+    global message
     global total_score
+    
     if not file_name.lower().endswith(".html"):
-        print("Error: The file is not an HTML file.")
-        print("Total Score: 0")
-        print("If you did not submit an HTML file, please email your assignment with the correct .html file ending to your TA so it can be graded.")
+        student_feedback += "Error: The file is not an HTML file.\n"
+        student_feedback += "Please email your correct assignment file to your TA so it can be graded."
+        message += "\nError: The file is not an HTML file."
         total_score -= 1000
         return
 
@@ -24,33 +27,39 @@ def check_type_html(file_name):
     
 # 1. Title (this is different than a heading) (50 points)
 def check_title():
+    global student_feedback
     global total_score
+    
     title_tag = soup.find("title")
     if title_tag is None:
-        print("-50: The file does not have a title.")
+        student_feedback += "-50: The file does not have a title.\n"
         total_score -= 50
         
 
 # 2. Headings of two different sizes (ex. h1, h2, h3, etc.) (50 points each)
 def check_headings():
+    global student_feedback
     global total_score
+    
     headings = soup.find_all(["h1", "h2", "h3", "h4", "h5", "h6"])
     heading_sizes = set()
     for heading in headings:
         heading_sizes.add(heading.name)
-    print(heading_sizes)
+    # print(heading_sizes)
 
     if len(heading_sizes) == 1:
-        print("-50: The file contains only headings of one size (instead of two different sizes): ", ", ".join(heading_sizes))
+        student_feedback += "-50: The file contains only headings of one size (instead of two different sizes): " + ", ".join(heading_sizes) + ".\n"
         total_score -= 50
         
     elif len(heading_sizes) < 1:
-        print("-100: The file does not contain at least two headings of different sizes.")
+        student_feedback += "-100: Does not have least two headings of different sizes.\n"
         total_score -= 100
 
 # 3. Add a tooltip (or "hover box") to your first use of the h1 tag. (50 points)
 def check_tooltip():
+    global student_feedback
     global total_score
+    
     h1_tag = soup.find("h1")
     if h1_tag:
         nested_tags_with_title = h1_tag.find_all(lambda tag: tag.has_attr("title"))
@@ -71,17 +80,19 @@ def check_tooltip():
             for css_style in css_styles:
                 if "h1:hover" in css_style or ".tooltip" in css_style:
                     return
-            print("-50: The first <h1> tag in the file does not have a tooltip or hover box.")
+            student_feedback += "-50: The first <h1> tag in the file does not have a tooltip or hover box.\n"
             total_score -= 50
             return
     else:
-        print("-50: The file does not contain any <h1> tags.")
+        student_feedback += "-50: No <h1> tags.\n"
         total_score -= 50
 
 
 # 4. Colored background (other than white) (100 points)
 def check_colored_background():
+    global student_feedback
     global total_score
+    
     # colored background set in the CSS style tag
     style_tags = soup.find_all("style")
     for style_tag in style_tags:
@@ -95,15 +106,17 @@ def check_colored_background():
             bgcolor = body_tag.get("bgcolor")
             if bgcolor:
                 return
-            elif body_tag.has_attr("style") and "background-color" in body_tag["style"]:
+            elif body_tag.has_attr("style") and ("background-color" in body_tag["style"] or "background-image" in body_tag["style"]):
                 return
-        print("-100: The file does not have a colored background.")
+        student_feedback += "-100: The file does not have a colored background.\n"
         total_score -= 100
 
 
 # 5. "Mailto" link to your email address (50 points)
 def check_mailto():
+    global student_feedback
     global total_score
+    
     a_tags = soup.find_all("a", href=True)
     for a_tag in a_tags:
         href = a_tag["href"]
@@ -115,13 +128,15 @@ def check_mailto():
                 # print(f"Found email address in "{file_name}": {email_address}")
             # else:
                 # print("No valid email address found.")
-    print(f"-50: The file does NOT have a \"mailto\" link to your email address.")
+    student_feedback += "-50: The file does not have a \"mailto\" link to your email address.\n"
     total_score -= 50
 
 
 # 6. Hyperlink to another website (must use a URL, not a file path) (50 points)
 def check_hyperlink():
+    global student_feedback
     global total_score
+    
     anchor_tags = soup.find_all("a", href=True)
 
     for anchor_tag in anchor_tags:
@@ -129,13 +144,15 @@ def check_hyperlink():
         # Check if the URL is a valid HTTP or HTTPS link
         if re.match(r"^https?://", url):
             return
-    print("-50: The file does not contain a hyperlink to another website.")
+    student_feedback += "-50: No hyperlink to another website.\n"
     total_score -= 50
 
 
 # 7. Bold text and italic text (50 points each)
 def check_bold_and_italic():
+    global student_feedback
     global total_score
+    
     bold_tags = soup.find_all(["b", "strong"]) # Find all bold and strong tags
     italic_tags = soup.find_all(["i", "em"]) # Find all italic and emphasis tags
     
@@ -149,26 +166,31 @@ def check_bold_and_italic():
     # check style tag styles
     style_tags = soup.find_all("style")
     for tag in style_tags:
-        if "font-weight:bold" or "font-weight: bold" in tag.text:
+        if "font-weight:bold" in tag.text or "font-weight: bold" in tag.text:
             bold = True
             break
     for tag in style_tags:
-        if "font-style:italic" or "font-style: italic" in tag.text:
+        if "font-style:italic" in tag.text or "font-style: italic" in tag.text:
             italic = True
             break
             
-    if not bold_tags and not len(elements_with_inline_bold_styles) < 1 and not bold:
-        print("-50: The file does not contain bold text.")
+    # print(not(bold_tags))
+    # print(len(elements_with_inline_bold_styles) < 1)
+    # print(bold)
+    if (not(bold_tags)) and (len(elements_with_inline_bold_styles) < 1) and (bold is False):
+        student_feedback += "-50: No bold text.\n"
         total_score -= 50
 
-    if not italic_tags and not len(elements_with_inline_italic_styles) < 1 and not italic:
-        print("-50: The file does not contain italic text.")
+    if (not(italic_tags)) and (len(elements_with_inline_italic_styles) < 1) and (italic is False):
+        student_feedback += "-50: No italic text.\n"
         total_score -= 50
 
 
 # 8. Centered text or photo (50 points)
 def check_centered_text_or_photo():
+    global student_feedback
     global total_score
+    
     style_tag = soup.find("style")
     if style_tag:
         style_text = style_tag.get_text()
@@ -181,21 +203,24 @@ def check_centered_text_or_photo():
     
     
     if not centered_elements_html and not centered_text_elements_css and not centered_img_elements_css:
-        print(f"-50: The file does not contain centered content.")
+        student_feedback += "-50: No centered content.\n"
         total_score -= 50
 
 
 # 9. Horizontal line (aka "horizontal rule") (50 points)
 def check_horizontal_line():
+    global student_feedback
     global total_score
+    
     horizontal_lines = soup.find_all("hr")
     if not horizontal_lines:
-        print("-50: The file does not contain horizontal lines (horizontal rules).")
+        student_feedback += "-50: No horizontal lines (horizontal rules).\n"
         total_score -= 50
 
 
 # 10. Ordered list (numbered list) and unordered list (bulleted list) (50 points each)
 def check_ordered_and_unordered_list():
+    global student_feedback
     global total_score
     
     # li tags without parent count as unordered lists, so don't deduct points if there are such li tags
@@ -209,17 +234,20 @@ def check_ordered_and_unordered_list():
     ordered_lists = soup.find_all("ol")
     unordered_lists = soup.find_all("ul")
     if not ordered_lists:
-        print("-50: The file does not contain ordered lists (numbered lists).")
+        student_feedback += "-50: No ordered lists (numbered lists).\n"
         total_score -= 50
 
     if check_unordered and not unordered_lists:
-        print("-50: The file does not contain unordered lists (bulleted lists).")
+        student_feedback += "-50: No unordered lists (bulleted lists).\n"
         total_score -= 50
 
 
 # 11. A working picture, hosted online. You can link to an existing photo or upload your own image to photo-sharing sites like Google Photos or imgur.com. Make sure the photo is shared properly, and test your page on someone else's device to be certain. (100 points)
 def check_picture():
+    global student_feedback
+    global message
     global total_score
+    
     picture_tags = soup.find_all("img")
 
     counter = 0
@@ -231,7 +259,7 @@ def check_picture():
 
         try:
             if src is None:
-                print("-100: The file does not contain a working picture hosted online.")
+                student_feedback += "-100: No working picture hosted online.\n"
                 total_score -= 100
                 return
             
@@ -258,20 +286,22 @@ def check_picture():
                 #     msg = True
                 elif response.status_code >= 300:
                     if counter == len(picture_tags): # last picture
-                        print("To grader: check manually if there is an image. If image not visible, please manually deduct 100 points.")
+                        message += "\nCheck manually if there is an image. If image not visible, please manually deduct 100 points."
                         return
         except Exception as e:
             if counter == len(picture_tags): # last picture
-                print("-100: The file does not contain a working picture hosted online.")
+                student_feedback += "-100: No working picture hosted online.\n"
                 total_score -= 100
                 return
-    print("-100: The file does not contain a working picture hosted online.")
+    student_feedback += "-100: No working picture hosted online.\n"
     total_score -= 100
 
 
 # 12. Set the width and height of your photo. Recommend 450x600 for portrait layout, or 600x450 for landscape. If your source photo isn't 4:3 aspect ratio, this may look odd. (100 points)
 def check_picture_width_height():
+    global student_feedback
     global total_score
+    
     picture_tags = soup.find_all("img")
 
     for picture_tag in picture_tags:
@@ -280,7 +310,7 @@ def check_picture_width_height():
         if width and height:
             return
         inline_style = picture_tag.get("style")
-        if inline_style and ("width" in inline_style) and ("height" in inline_style):
+        if inline_style and (("width" in inline_style) or ("height" in inline_style)):
             return
     
     # find all <style> tags to extract CSS
@@ -291,18 +321,20 @@ def check_picture_width_height():
 
     # check if CSS contains width and height styles for img and image classes
     for css_style in css_styles:
-        if "img" in css_style and ("width" in css_style and "height" in css_style):
+        if "img" in css_style and ("width" in css_style or "height" in css_style):
             return
-        if "image" in css_style and ("width" in css_style and "height" in css_style):
+        if "image" in css_style and ("width" in css_style or "height" in css_style):
             return
     
-    print("-100: The file does not contain an image with both width and height attributes set.")
+    student_feedback += "-100: Either the image does not have width and height set, or there is no image.\n"
     total_score -= 100
 
 
 # 13. Add a comment at the very bottom of your source code, listing the tools you used to create this: operating system, text editor, and the web browser you used to test it. (100 points)
 def check_comment():
+    global student_feedback
     global total_score
+    
     comments = soup.find_all(string=lambda text: isinstance(text, Comment))
     for comment in comments:
         words = comment.strip().split()
@@ -314,54 +346,84 @@ def check_comment():
     if len(matches) > 0:
         return
 
-    print("-100: The file does NOT contain a comment for the tools used.")
+    student_feedback += "-100: No comment for the tools used.\n"
     total_score -= 100
 
 
 # Extra Credit: Add a video to your page, using YouTube, Vimeo, or other video hosting service. Find a video, look for the "Share" option, and select "Embed" or "Link" to find a working video link.
 def check_extra_credit_video():
+    global student_feedback
+    global message
     global total_score
+    global extra_credit_done
+    global unsure_about_extra_credit
+    
     iframe_tags = soup.find_all("iframe")
+    embed_tags = soup.find_all("embed")
 
     for iframe_tag in iframe_tags:
         src = iframe_tag.get("src")
         if src and ("youtube.com/embed" in src or "player.vimeo.com/video" in src):
-            print("+100 extra credit for embedding a video.")
+            student_feedback += "+100 extra credit for embedding a video."
             # print("Video URL:", src)
             total_score += 100
+            extra_credit_done = True
             return
-
+        if src and ("youtube.com/watch" in src):
+            student_feedback += "No extra credit: video is not correctly embedded."
+            return
+        else:
+            message += "\nAutograder cannot tell if video is correctly embedded. Please manually view the HTML file verify."
+            unsure_about_extra_credit = True
+            return
+    
+    for embed_tag in embed_tags:
+        src = embed_tag.get("src")
+        if src and ("youtube.com/embed" in src or "player.vimeo.com/video" in src):
+            student_feedback += "+100 extra credit for embedding a video."
+            # print("Video URL:", src)
+            total_score += 100
+            extra_credit_done = True
+            return
+        if src and ("youtube.com/watch" in src): 
+            student_feedback += "No extra credit: video is not correctly embedded."
+            return
+        else:
+            message += "\nAutograder cannot tell if video is correctly embedded. Please manually view the HTML file verify."
+            unsure_about_extra_credit = True
+            return
 
 if __name__ == "__main__":
     
     output_file = "student_scores.csv"
     with open(output_file, "w", newline="") as csvfile:
         writer = csv.writer(csvfile)
+        column_headers = ["lastnamefirstname", "Total Score", "Student Feedback", "Message to TA/Grader"]
+        writer.writerow(column_headers)
 
         folder_path = "./TA2_Submissions/"
         submissions_folder = sorted(os.listdir(folder_path)) # Get a list of all files in the folder in alphabetical order
         for file_name in submissions_folder:
             total_score = 1000
+            extra_credit_done = False
+            unsure_about_extra_credit = False
+            message = ""
+            student_feedback = ""
             
             file_path = os.path.join(folder_path, file_name)
             
             print("file_path:", file_path)
+            message += "file_path: " + file_path
             parts = file_name.split("_")
             prefix = parts[0] # prefix (before 1st underscore) is lastnamefirstname of student
             
             late = False
             # print("parts:", parts)
             if parts[1] == "LATE":
-                print("-100: late submission")
-                print("Note: Manually check submission time. if submitted within 1 hour after due date, then remove the late penalty")
+                student_feedback += "-100: late submission\n"
+                message += "\nLate submission. Please check the submission time. if submitted within 1 hour after due date, then remove the late penalty."
                 total_score -= 100
             
-            message = ""
-            if total_score <= 1000:
-                message = "Total score is <= 1000. Good idea to manually look at submission to ensure grading script made no errors."
-            elif total_score == 1100:
-                message = "Good job! :)"
-                
 
             soup = check_type_html(file_path)
             if soup:
@@ -380,6 +442,14 @@ if __name__ == "__main__":
                 check_comment() # 13.
                 check_extra_credit_video()
             
-            print("\n\n\n")
+            
+            if total_score <= 700:
+                message += "\nTotal score is <= 700. Good idea to manually check the submission to ensure the grading script made no errors."
+            elif total_score == 1100:
+                student_feedback += "\nGood job! :)"
+            elif total_score == 1000 and extra_credit_done is False:
+                student_feedback += "Good job! :)"
 
-            writer.writerow([prefix, total_score])
+            
+            # write to csv file
+            writer.writerow([prefix, total_score, student_feedback, message])
